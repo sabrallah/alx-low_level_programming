cc8261cd2819d58a305fde5d5575b1fdc41d5f81
sabrallah <sabrallah1991@gmail.com>
2023-03-18 14:17:10 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,12 @@
 #include <unistd.h>
+/**
+ * main - 101-qoute.c
+ * description - Write a C program that prints exactly and that piece of
+ * art is useful\"
+ *  - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
         write(2,
