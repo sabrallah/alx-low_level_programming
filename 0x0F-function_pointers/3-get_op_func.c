8290a9a5c4096128bc41e85faf8ad49d5b48b0ab
sabrallah <sabrallah1991@gmail.com>
2023-04-19 05:51:37 -0700

    task3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -3,7 +3,7 @@
 /**
  * get_op_func - selects the correct function to perform the operation
  * @s: operator passed as argument
- * Return: pointer to the function corresponding to the operator
+ * Return: pointer to the function that corresponds to the operator
  */
 int (*get_op_func(char *s))(int, int)
 {
@@ -15,15 +15,15 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-	int i;
+	int i = 0;
 
-	for (i = 0; ops[i].op != NULL; i++)
+	while (ops[i].op != NULL)
 	{
-		if (strcmp(s, ops[i].op) == 0)
-		{
-			return (ops[i].f);
-		}
+		if (s[0] == ops[i].op[0] && s[1] == '\0')
+		return (ops[i].f);
+		i++;
 	}
 
 	return (NULL);
 }
+
