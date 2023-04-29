a71e30f9c4a73f7e40145410da456b54fc92b943
sabrallah <sabrallah1991@gmail.com>
2023-04-29 03:26:46 -0700

    task0

---
+++ 0x13-more_singly_linked_lists/lists.h
@@ -0,0 +1,23 @@
+#ifndef LISTS_H
+#define LISTS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * struct listint_s - Singly linked list
+ * @n: Integers
+ * @next: Pointter to the nexts node
+ *
+ * Description: Singly linked lisst node structure
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
+/* Function prototypes */
+size_t print_listint(const listint_t *h);
+
+#endif /* LISTS_H */
