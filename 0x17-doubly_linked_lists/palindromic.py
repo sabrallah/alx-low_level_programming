66779b52495f656bd2ed0081d03d8d394347b901
sabrallah <sabrallah1991@gmail.com>
2023-06-15 09:16:17 -0700

    task10

--- 0x17-doubly_linked_lists/palindromic.py
+++ 0x17-doubly_linked_lists/palindromic.py
@@ -1,25 +1,13 @@
 #!/usr/bin/python3
 
-def is_palindrome(n):
-    return str(n) == str(n)[::-1]
-
-
-num_digits = int(input("Enter the number of digits: "))
-
-
-lower_limit = 10 ** (num_digits - 1)
-upper_limit = 10 ** num_digits
-
-
 largest_palindrome = 0
 
-
-for i in range(lower_limit, upper_limit):
-    for j in range(lower_limit, upper_limit):
+for i in range(999, 99, -1):
+    for j in range(i, 99, -1):
         product = i * j
-        if is_palindrome(product) and product > largest_palindrome:
+        if str(product) == str(product)[::-1] and product > largest_palindrome:
             largest_palindrome = product
 
-
-with open("102-result", "w") as file:
+# Save the result in the file
+with open('102-result', 'w') as file:
     file.write(str(largest_palindrome))
