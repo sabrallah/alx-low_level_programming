3f7d9050187e6b27d0664d4b07de1768b48cfe45
sabrallah <sabrallah1991@gmail.com>
2023-06-19 03:11:34 -0700

    create file.c

---
+++ 0x18-dynamic_libraries/3-strspn.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: string to search
+ * @accept: string containing the characters to match
+ *
+ * Return: number of bytes in the initial segment of s which consist only of
+ * bytes from accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+				break;
+		}
+		if (!accept[j])
+			break;
+	}
+	return (i);
+}
