8afd11f4254dfcb12f14ed6fb55f170500cf60a2
sabrallah <sabrallah1991@gmail.com>
2023-08-23 08:23:23 -0700

    task5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -3,23 +3,25 @@
 This script computes the perimeter of an island within a grid.
 """
 
-def count_water_neighbors(grid, s, j):
+
+def count_water_neighbors(grid, i, j):
     """
     Counts the number of water neighbors for a cell in a given grid.
     """
 
     count = 0
 
-    if s <= 0 or not grid[s - 1][j]:
+    if i <= 0 or not grid[i - 1][j]:
         count += 1
-    if j <= 0 or not grid[s][j - 1]:
+    if j <= 0 or not grid[i][j - 1]:
         count += 1
-    if j >= len(grid[s]) - 1 or not grid[s][j + 1]:
+    if j >= len(grid[i]) - 1 or not grid[i][j + 1]:
         count += 1
-    if s >= len(grid) - 1 or not grid[s + 1][j]:
+    if i >= len(grid) - 1 or not grid[i + 1][j]:
         count += 1
 
     return count
+
 
 def calculate_island_perimeter(grid):
     """
@@ -27,9 +29,9 @@ def calculate_island_perimeter(grid):
     """
 
     perimeter = 0
-    for s in range(len(grid)):
-        for j in range(len(grid[s])):
-            if grid[s][j]:
-                perimeter += count_water_neighbors(grid, s, j)
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j]:
+                perimeter += count_water_neighbors(grid, i, j)
 
     return perimeter
